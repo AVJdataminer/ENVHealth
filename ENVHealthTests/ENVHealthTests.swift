594268f4ef4f0a4fecbc2803ce9a9ec1c28b20de
Avjdataminer <aiden.dataminer@gmail.com>
2025-08-28 15:20:41 -0600

    Initial Commit

---
+++ ENVHealthTests/ENVHealthTests.swift
@@ -0,0 +1,16 @@
+//
+//  ENVHealthTests.swift
+//  ENVHealthTests
+//
+//  Created by Aiden Johnson on 8/28/25.
+//
+
+import Testing
+
+struct ENVHealthTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
